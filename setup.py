d3e55c63a0c5e90d6ed49068a0989578dc3d25c2
Patrick Harrison <patrick.o.harrison@gmail.com>
2020-02-21 14:42:32 -0600

    Created base setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="ayyrpc",
+    version="0.1",
+    description="Simple asyncio-ready RPC prodocol",
+    author="Patrick Harrison",
+    author_email="patrick.o.harrison@gmail.com",
+    url="https://github.com/patrick-o-harrison/ayyrpc",
+    py_modules=["ayyrpc"],
+)
